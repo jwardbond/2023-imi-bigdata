a8ac657d4a9c1e6e9d32aef30d1e54ebdd8e150c
Jesse Ward-Bond <jessewb.c@gmail.com>
2024-03-16 20:53:47 -0400

    Refarctored to flag external names

--- webapp/app.py
+++ webapp/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 cors = CORS(app)
 
 # getting the data
-nodes = pd.read_parquet("../data/processed/nodes.parquet")
+nodes = pd.read_parquet("../data/processed/nodes_rev.parquet")
 cash = pd.read_parquet("../data/processed/cash.parquet")
 emt = pd.read_parquet("../data/processed/emt.parquet")
 wire = pd.read_parquet("../data/processed/wire.parquet")
